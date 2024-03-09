3a8b0011e6c6a9ba9848e3565f4409739b587efc
Colton Hye <hyercolt@msu.edu>
2024-03-09 11:05:25 -0500

    lol

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add JavaScript below
+// script.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the modal element
+    var modal = document.getElementById("modal");
+  
+    // Get the modal image element
+    var modalImage = document.getElementById("modalImage");
+  
+    // Get all images on the page
+    var images = document.querySelectorAll("img");
+  
+    // Attach click event listener to each image
+    images.forEach(function(image) {
+      image.addEventListener("click", function() {
+        // Set the src attribute of the modal image to the clicked image's src
+        modalImage.src = this.src;
+        // Show the modal
+        modal.classList.remove("hidden");
+      });
+    });
+  
+    // Hide the modal when clicking outside of it
+    modal.addEventListener("click", function(event) {
+      if (event.target === modal) {
+        modal.classList.add("hidden");
+      }
+    });
+  });
+  
